4c4d144e1bb3dfe08593c8886d2fdca388e468aa
陈郑游 <649954910@qq.com>
2018-12-06 11:56:33 +0800

    版本-3.0

--- commons-utils/src/main/java/excelUtil/ExcelUtils.java
+++ commons-utils/src/main/java/excelUtil/ExcelUtils.java
@@ -23,14 +23,11 @@ import static org.apache.poi.ss.util.CellUtil.createCell;
 /**
  * @author Chenzy
  * @date 2018-05-03
- * @description: Excel 操作相关工具类
+ * @description: Excel 导入导出操作相关工具类
+ * @Email: 649954910@qq.com
  */
 public class ExcelUtils {
 
-    /**
-     * 更新日志：
-     * 1.解决 SimpleDateFormat 与 DecimalFormat 线程安全问题[2018-11-07]。
-     */
     private static final ThreadLocal<SimpleDateFormat> fmt = new ThreadLocal<>();
     private static final String MESSAGE_FORMAT = "yyyy-MM-dd";
     private static final String DataValidationError1 = "提示信息：";
@@ -138,19 +135,20 @@ public class ExcelUtils {
      * 功能描述: excel 数据导出、导出模板
      * <p>
      * 更新日志:
-     * 1.response.reset();注释掉reset，否在会出现跨域错误。
-     * 2.新增导出多个单元。
-     * 3.poi官方建议大数据量解决方案：SXSSFWorkbook。
-     * 4.自定义下拉列表：对每个单元格自定义下拉列表。
-     * 5.数据遍历方式换成数组(效率较高)。
-     * 6.可提供模板下载。
+     * 1.response.reset();注释掉reset，否在会出现跨域错误。[2018-05-18]
+     * 2.新增导出多个单元。[2018-08-08]
+     * 3.poi官方建议大数据量解决方案：SXSSFWorkbook。[2018-08-08]
+     * 4.自定义下拉列表：对每个单元格自定义下拉列表。[2018-08-08]
+     * 5.数据遍历方式换成数组(效率较高)。[2018-08-08]
+     * 6.可提供模板下载。[2018-08-08]
      * 7.每个表格的大标题[2018-09-14]
      * 8.自定义列宽：对每个单元格自定义列宽[2018-09-18]
-     * 9.自定义样式：对每个单元格自定义样式[2018-10-22][2018-10-25修复][2018-11-12添加行高]
+     * 9.自定义样式：对每个单元格自定义样式[2018-10-22]
      * 10.自定义单元格合并：对每个单元格合并[2018-10-22]
      * 11.固定表头[2018-10-23]
-     * 12.自定义样式：单元格自定义某一列或者某一行样式[2018-10-30][2018-11-12添加行高]
+     * 12.自定义样式：单元格自定义某一列或者某一行样式[2018-11-12]
      * 13.忽略边框(默认是有边框)[2018-11-15]
+     * 14.函数式编程换成面向对象编程[2018-12-06-5]
      * <p>
      * 版  本:
      * 1.apache poi 3.17
@@ -198,8 +196,6 @@ public class ExcelUtils {
         System.out.println("======= Excel 工具类导出运行时间:  " + (System.currentTimeMillis() - startTime) + " ms");
         return true;
     }
-
-
 
 
     /**
@@ -303,7 +299,13 @@ public class ExcelUtils {
      */
     private static void setDataList(SXSSFWorkbook sxssfWorkbook, SXSSFRow sxssfRow, List<List<String[]>> dataLists, HashMap notBorderMap,
                                     HashMap regionMap, HashMap columnMap, HashMap styles, HashMap paneMap,
-                                    String[] sheetName, String[] labelName, HashMap rowStyles, HashMap columnStyles, HashMap dropDownMap) {
+                                    String[] sheetName, String[] labelName, HashMap rowStyles, HashMap columnStyles, HashMap dropDownMap) throws Exception {
+        if (dataLists == null) {
+            throw new IOException("Andyczy ExcelUtils Exception Message：Export data(type:List<List<String[]>>) cannot be empty ！");
+        }
+        if (sheetName == null) {
+            throw new IOException("Andyczy ExcelUtils Exception Message：Export sheet(type:String[]) name cannot be empty ！");
+        }
         int k = 0;
         for (List<String[]> list : dataLists) {
             SXSSFSheet sxssfSheet = sxssfWorkbook.createSheet();
@@ -379,31 +381,29 @@ public class ExcelUtils {
      * @throws Exception
      */
     private static void setIo(SXSSFWorkbook sxssfWorkbook, OutputStream outputStream, String fileName, String[] sheetName, HttpServletResponse response, String filePath) throws Exception {
-        if (response != null && filePath == null) {
-            //  设置响应头信息。
-            response.setHeader("Charset", "UTF-8");
-            response.setHeader("Content-Type", "application/force-download");
-            response.setHeader("Content-Type", "application/vnd.ms-excel");
-            response.setHeader("Content-disposition", "attachment; filename=" + URLEncoder.encode(fileName == null ? sheetName[0] : fileName, "utf8") + ".xlsx");
-            response.flushBuffer();
-            outputStream = response.getOutputStream();
-        } else {
-            new Exception("response and filePath parameter Not empty !");
+        try {
+            if (response != null) {
+                response.setHeader("Charset", "UTF-8");
+                response.setHeader("Content-Type", "application/force-download");
+                response.setHeader("Content-Type", "application/vnd.ms-excel");
+                response.setHeader("Content-disposition", "attachment; filename=" + URLEncoder.encode(fileName == null ? sheetName[0] : fileName, "utf8") + ".xlsx");
+                response.flushBuffer();
+                outputStream = response.getOutputStream();
+            } else if (filePath != null) {
+                outputStream = new FileOutputStream(filePath);
+            }
+            if (sxssfWorkbook != null) {
+                sxssfWorkbook.write(outputStream);
+                sxssfWorkbook.dispose();
+            }
+        } catch (Exception e) {
+            e.getSuppressed();
         }
-        if (response == null && filePath != null) {
-            outputStream = new FileOutputStream(filePath);
-        } else {
-            new Exception("response and filePath parameter Not empty !");
-        }
-        sxssfWorkbook.write(outputStream);
-        //  处理在磁盘上支持此工作簿的临时文件。
-        sxssfWorkbook.dispose();
     }
 
 
     /**
      * 判断字符串是否为空
-     * 源码：只是为了该类不引入其他 jar 包
      *
      * @param str
      * @return
@@ -423,7 +423,8 @@ public class ExcelUtils {
     }
 
     /**
-     * 锁定行（固定表头）
+     * 功能描述: 锁定行（固定表头）
+     * 使用的方法：
      * HashMap setPaneMap = new HashMap();
      * setPaneMap.put(1,3);  //第一个表格、第三行开始固定表头
      *
@@ -441,9 +442,9 @@ public class ExcelUtils {
      * 使用的方法：
      * HashMap<Integer,HashMap<Integer,Integer>>  mapSheet = new HashMap();
      * HashMap<Integer,Integer> mapColumn = new HashMap();
-     * mapColumn.put(0,5);           //第一列，列宽为5
-     * mapColumn.put(3,5);           //第四列，列宽为5
-     * mapSheet.put(1, mapColumn);    //第一个元格列宽
+     * mapColumn.put(0,5);             //第一列，列宽为5
+     * mapColumn.put(3,5);             //第四列，列宽为5
+     * mapSheet.put(1, mapColumn);     //第一个元格列宽
      *
      * @param sxssfSheet
      * @param map
@@ -462,12 +463,11 @@ public class ExcelUtils {
 
 
     /**
-     * 功能描述:
-     * 1.excel 合并单元格
-     * 参数说明：
+     * 功能描述: excel 合并单元格
+     * 使用的方法：
      * List<List<Integer[]>> setMergedRegion = new ArrayList<>();
      * List<Integer[]> sheet1 = new ArrayList<>();                  //第一个表格设置。
-     * Integer[] sheetColumn1 = new Integer[]{0, 1, 0, 2}         //代表起始行号，终止行号， 起始列号，终止列号进行合并。（excel从零行开始数）
+     * Integer[] sheetColumn1 = new Integer[]{0, 1, 0, 2}           //代表起始行号，终止行号， 起始列号，终止列号进行合并。（excel从零行开始数）
      * setMergedRegion.add(sheet1);
      *
      * @param sheet
@@ -489,7 +489,7 @@ public class ExcelUtils {
     }
 
     /**
-     * 合并单元格
+     * 功能描述: 合并单元格
      *
      * @param sheet
      * @param firstRow
@@ -504,17 +504,18 @@ public class ExcelUtils {
 
     /**
      * 功能描述:下拉列表
+     * 使用的方法：
+     * HashMap hashMap = new HashMap();
+     * List<String[]> sheet1 = new ArrayList<>();                  //第一个表格设置。
+     * String[] sheetColumn1 = new String[]{"1", "2", "4"};        //必须放第一：设置下拉列表的列（excel从零行开始数）
+     * String[] sex = {"男","女"};                                  //下拉的值放在 sheetColumn1 后面。
+     * sheet1.add(sheetColumn1);
+     * sheet1.add(sex);
+     * hashMap.put(1,sheet1);                                      //第一个表格的下拉列表值
      *
      * @param sheet
      * @param dropDownListData
-     * @param dataListSize     参数说明：
-     *                         HashMap hashMap = new HashMap();
-     *                         List<String[]> sheet1 = new ArrayList<>();                  //第一个表格设置。
-     *                         String[] sheetColumn1 = new String[]{"1", "2", "4"};        //必须放第一：设置下拉列表的列（excel从零行开始数）
-     *                         String[] sex = {"男,女"};                                   //下拉的值放在 sheetColumn1 后面。
-     *                         sheet1.add(sheetColumn1);
-     *                         sheet1.add(sex);
-     *                         hashMap.put(1,sheet1);                                      //第一个表格的下拉列表值
+     * @param dataListSize
      */
     private static void setDataValidation(SXSSFSheet sheet, List<String[]> dropDownListData, int dataListSize) {
         if (dropDownListData.size() > 0) {
@@ -554,9 +555,9 @@ public class ExcelUtils {
 
 
     /**
-     * 所有数据的样式(全加边框、不加粗、12号字体、居中、黑色字体)
+     * 样式
      *
-     * @param notBorderMap 忽略边框
+     * @param notBorderMap
      * @param cell
      * @param wb
      * @param sxssfRow
@@ -627,18 +628,19 @@ public class ExcelUtils {
     }
 
     /**
-     * 自定义某一行、列的样式
+     * 功能描述：自定义某一行、列的样式
+     * 使用的方法：是否居中？，是否右对齐？，是否左对齐？， 是否加粗？，是否有边框？ —— 颜色、字体、行高？
+     * HashMap hashMap = new HashMap();
+     * List list = new ArrayList();
+     * list.add(new Boolean[]{true, false, false, false, true});                //1、样式
+     * list.add(new Integer[]{1, 3});                                           //2、第几行或者是第几列
+     * list.add(new Integer[]{10,14,null});                                     //3、颜色值 、字体大小、行高（可不设置）
+     * hashMap.put(1,list);                                                     //第一表格
      *
      * @param cell
      * @param wb
      * @param rowstyleList
-     * @param rowIndex     说明：是否居中？，是否右对齐？，是否左对齐？， 是否加粗？，是否有边框？ —— 颜色、字体、行高？
-     *                     HashMap hashMap = new HashMap();
-     *                     List list = new ArrayList();
-     *                     list.add(new Boolean[]{true, false, false, false, true});                //1、样式
-     *                     list.add(new Integer[]{1, 3});                                           //2、第几行或者是第几列
-     *                     list.add(new Integer[]{10,14,null});                                     //3、颜色值 、字体大小、行高（可不设置）
-     *                     hashMap.put(1,list);                                                     //第一表格
+     * @param rowIndex
      */
     private static void setExcelCellStyles(Cell cell, SXSSFWorkbook wb, SXSSFRow sxssfRow, List<Object[]> rowstyleList, int rowIndex) {
         if (rowstyleList != null && rowstyleList.size() > 0) {
@@ -667,25 +669,28 @@ public class ExcelUtils {
     }
 
     /**
+     * 功能描述：所有自定义单元格样式
+     * 使用的方法：是否居中？，是否右对齐？，是否左对齐？， 是否加粗？，是否有边框？  —— 颜色、字体、行高？
+     * HashMap cellStyles = new HashMap();
+     * List< List<Object[]>> list = new ArrayList<>();
+     * List<Object[]> objectsList = new ArrayList<>();
+     * List<Object[]> objectsListTwo = new ArrayList<>();
+     * objectsList.add(new Boolean[]{true, false, false, false, true});      //1、样式一（必须放第一）
+     * objectsList.add(new Integer[]{10, 12});                               //1、颜色值 、字体大小、行高（必须放第二）
+     * <p>
+     * objectsListTwo.add(new Boolean[]{false, false, false, true, true});   //2、样式二（必须放第一）
+     * objectsListTwo.add(new Integer[]{10, 12,null});                       //2、颜色值 、字体大小、行高（必须放第二）
+     * <p>
+     * objectsList.add(new Integer[]{5, 1});                                 //1、第五行第一列
+     * objectsList.add(new Integer[]{6, 1});                                 //1、第六行第一列
+     * <p>
+     * objectsListTwo.add(new Integer[]{2, 1});                              //2、第二行第一列
+     * <p>
+     * cellStyles.put(1, list);                                              //第一个表格所有自定义单元格样式
+     *
      * @param cell
      * @param wb
-     * @param styles 是否居中？，是否右对齐？，是否左对齐？， 是否加粗？，是否有边框？  —— 颜色、字体、行高？
-     *               HashMap cellStyles = new HashMap();
-     *               List< List<Object[]>> list = new ArrayList<>();
-     *               List<Object[]> objectsList = new ArrayList<>();
-     *               List<Object[]> objectsListTwo = new ArrayList<>();
-     *               objectsList.add(new Boolean[]{true, false, false, false, true});      //1、样式一（必须放第一）
-     *               objectsList.add(new Integer[]{10, 12});                               //1、颜色值 、字体大小、行高（必须放第二）
-     *               <p>
-     *               objectsListTwo.add(new Boolean[]{false, false, false, true, true});   //2、样式二（必须放第一）
-     *               objectsListTwo.add(new Integer[]{10, 12,null});                       //2、颜色值 、字体大小、行高（必须放第二）
-     *               <p>
-     *               objectsList.add(new Integer[]{5, 1});                                 //1、第五行第一列
-     *               objectsList.add(new Integer[]{6, 1});                                 //1、第六行第一列
-     *               <p>
-     *               objectsListTwo.add(new Integer[]{2, 1});                              //2、第二行第一列
-     *               <p>
-     *               cellStyles.put(1, list);                                              //第一个表格所有自定义单元格样式
+     * @param styles
      */
     private static void setExcelStyles(Cell cell, SXSSFWorkbook wb, SXSSFRow sxssfRow, List<List<Object[]>> styles, int cellIndex, int rowIndex) {
         if (styles != null) {
@@ -723,7 +728,7 @@ public class ExcelUtils {
 
 
     /**
-     * 设置边框
+     * 功能描述：设置边框
      *
      * @param cellStyle
      * @param isBorder
